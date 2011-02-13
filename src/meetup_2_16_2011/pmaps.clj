81d3da4f015f270a83f23d4a138dc84384ff746f
Julien Chastang <julien.c.chastang@gmail.com>
2011-02-12 21:40:14 -0700

    pmap example.

---
+++ src/meetup_2_16_2011/pmaps.clj
@@ -0,0 +1,19 @@
+(ns meetup-2-16-2011.pmaps)
+
+(defn long-f [f & args]
+  (Thread/sleep 1000)
+  (apply f args))
+
+(defn sqr [x] (* x x))
+
+
+(map #(sqr %) (range 0 10))
+
+(time (def a (doall (map #(long-f sqr %) (range 0 10)))))
+
+
+;; Now try with pmap
+
+(time (def b (doall (pmap #(long-f sqr %) (range 0 10)))))
+
+;; Under the hood pmap uses futures
